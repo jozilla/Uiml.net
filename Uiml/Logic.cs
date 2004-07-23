74b6362ef76d6f31967b1fc25b8f249e9d9d44ab
jo_vermeulen <>
2004-07-23 10:07:50 +0000

    Removed XPath expressions from Logic class.

--- Uiml/Logic.cs
+++ Uiml/Logic.cs
@@ -28,14 +28,9 @@ namespace Uiml{
 	using System.Xml.XPath;
 	using System.Collections;
 
-
-	//TODO
-	//Redundancy with vocabulary has to be removed!!!
-	//Will merge the duplicated functionality in a single class
 	public class Logic : UimlAttributes, IUimlElement
 	{
-
-		private XPathNavigator m_logicDocument;
+		private XmlNode m_tag;
 
 		public Logic()
 		{
@@ -44,93 +39,24 @@ namespace Uiml{
 
 		public Logic(XmlNode n) : this() 
 		{
+			m_tag = n;
 			Process(n);
 		}
 
 		public void Process(XmlNode n)
 		{
 			if(n.Name == IAM)
-			{
-				base.ReadAttributes(n);
-				m_logicDocument = n.CreateNavigator();
-			}
-		}
-
-		//WARNING TODO remove redundant code from Vocabulary: this code
-		//is identical
-		public const string GetMethodParamsExpr_1 = "//d-component[@id='";
-		public const string GetMethodParamsExpr_2 =  "']/d-method[@id='";
-		public const string GetMethodParamsExpr_3 = "']/d-param";
-		public Param[] GetMethodParams(string componentName, string methodName)
-		{
-			string xpexprstr =  GetMethodParamsExpr_1 + componentName + GetMethodParamsExpr_2 + methodName + GetMethodParamsExpr_3;
-			XPathExpression xpExpr = m_logicDocument.Compile(xpexprstr);
-			XPathExpression xpCountExpr = m_logicDocument.Compile("count(" + xpexprstr + ")");
-			
-			int nrp = Int32.Parse(m_logicDocument.Evaluate(xpCountExpr).ToString());
-			if(nrp == 0)
-				return new Param[0];
-		
-			XPathNodeIterator xpni = m_logicDocument.Select(xpExpr);
-			if(!xpni.MoveNext())
-			{
-				throw new MappingNotFoundException("Parameters of " + methodName + ":" + componentName);
-			}
-			else
-			{
-				Param[] parameters = new Param[nrp];
-				int i = 0;
-				do
-				{
-					Param p = new Param();
-					XPathNavigator xpnParam = xpni.Current.Clone();
-				
-					p.Identifier = xpnParam.GetAttribute(Param.ID, xpnParam.NamespaceURI); 
-					p.Type       = xpnParam.GetAttribute(Param.TYPE, xpnParam.NamespaceURI);
-				   parameters[i++] = p;
-				}
-				while(xpni.MoveNext());
-				
-				return parameters;
-			}
-
-		}
-
-		private string SelectFirst(XPathExpression xpExpr, string searchingFor)
-		{
-			XPathNodeIterator xpni = m_logicDocument.Select(xpExpr);
-			
-			if(!xpni.MoveNext())
-			{
-				throw new MappingNotFoundException(searchingFor);
-			}
-			else
-			{
-				return xpni.Current.Value;
-			}
-		}
-
-		public const string MapsOnComponentExpr_1 = "//d-component[@id='";
-		public const string MapsOnExpr_2 = "']/@maps-to";
-		public string MapsOnComponent(string abstractName)
-		{
-			XPathExpression xpExpr = m_logicDocument.Compile(MapsOnComponentExpr_1 + abstractName + MapsOnExpr_2);
-			return SelectFirst(xpExpr, abstractName);
-		}
-
-		
-		public const string GetMethodExpr_2 = "']/d-method[@id='";
-		public const string GetMethodExpr_3 = "']/@maps-to";
-		public const string GetMethodComponentExpr_1 = "//d-component[@id='";
-		public string GetMethodComponent(string identifier, string abstractName)
-		{
-			XPathExpression xpExpr = m_logicDocument.Compile(GetMethodComponentExpr_1 + abstractName + GetMethodExpr_2 + identifier + GetMethodExpr_3);
-			return SelectFirst(xpExpr, identifier + ":" + abstractName);
+				base.ReadAttributes(n);					
 		}
 
 		public ArrayList Children
 		{
 			get { return null; }
+		}
+
+		public XmlNode Tag
+		{
+			get { return m_tag; }
 		}
 
 		public const string IAM = "logic";
