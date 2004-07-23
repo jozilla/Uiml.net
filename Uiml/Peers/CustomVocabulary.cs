0d19bed23a05b2c233e7c35121d8382192f2bcf8
jo_vermeulen <>
2004-07-23 09:47:29 +0000

    Moved CustomVocabulary to Peers namespace.

--- Uiml/Peers/CustomVocabulary.cs
+++ Uiml/Peers/CustomVocabulary.cs
@@ -1,13 +1,13 @@
 /*
- 	 uiml.net: a uiml.ney renderer (http://lumumba.luc.ac.be/kris/research/uiml.net)
-    
+    Uiml.Net: a .Net UIML renderer (http://lumumba.luc.ac.be/kris/research/uiml.net)
+
 	 Copyright (C) 2003  Kris Luyten (kris.luyten@luc.ac.be)
 	                     Expertise Centre for Digital Media (http://edm.luc.ac.be)
 								Limburgs Universitair Centrum
 
 	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
+	modify it under the terms of the GNU Lesser General Public License
+	as published by the Free Software Foundation; either version 2.1
 	of	the License, or (at your option) any later version.
 
 	This program is distributed in the hope that it will be useful,
@@ -15,20 +15,20 @@
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
 
-	You should have received a copy of the GNU General Public License
+	You should have received a copy of the GNU Lesser General Public License
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-
-namespace Uiml{
+namespace Uiml.Peers
+{
 
 	using System;
 	using System.Xml;
 	using System.Xml.XPath;
 	using System.IO;
 
-	public class CustomVocabulary : Vocabulary{
+	public class CustomVocabulary : Vocabulary {
 
 		public CustomVocabulary()
 		{}
@@ -40,22 +40,21 @@ namespace Uiml{
 
 		private void Load(string idName, XmlNode subDoc)
 		{
-			UimlVocabulary = subDoc.CreateNavigator();
+			//TODO
+			//UimlVocabulary = subDoc.CreateNavigator();
 		}
 
 		public string QueryMapsTo(string name)
 		{
+			//TODO
+			return "dummy";
+			
+			/*
 			XPathNodeIterator  xpnn = m_vocabulary.Select("//descendant-or-self::component[@name='" + name + "'][last()]");
 			while(xpnn.MoveNext())
 				;
-			return xpnn.Current.GetAttribute(MAPSTO, "");	
-		}
-
-
-		public XPathNavigator UimlVocabulary
-		{
-			get { return m_vocabulary; }
-			set { m_vocabulary = value; }
+			return xpnn.Current.GetAttribute(MAPSTO, "");
+			*/
 		}
 
 		public const string MAPSTO = "maps-to";
@@ -63,4 +62,3 @@ namespace Uiml{
 	}
 
 }
-
