848ae39cef612e0013c9af00bfc5a91af12d3fad
jo_vermeulen <>
2005-11-07 14:33:52 +0000

    Updated library reference in SwfGUI and updated Makefile to copy frontend UIML files to output directory.

--- Uiml/FrontEnd/SwfGUI.cs
+++ Uiml/FrontEnd/SwfGUI.cs
@@ -39,7 +39,7 @@ namespace Uiml.FrontEnd
 	public class SwfGUI : UimlFrontEnd
 	{
 		static public string UIMLFILE = @"compactgui.uiml";
-		static public string UIMLLIB=@"uimldotnet.dll";
+		static public string UIMLLIB=@"uiml.net.dll";
 		public const string SWF_ASSEMBLY = "System.Windows.Forms";
 
 		public SwfGUI() : base(UIMLFILE, UIMLLIB)
