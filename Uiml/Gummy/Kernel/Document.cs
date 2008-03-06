511dff5ab58d99cef3765e356f211d11649fea22
Jo Vermeulen <jo.vermeulen@uhasselt.be>
2008-03-06 13:44:31 +0100

    Added (empty) Document class.

---
+++ Uiml/Gummy/Kernel/Document.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uiml.Gummy.Kernel
+{
+    /// <summary>
+    /// This class represents the current document in Gummy.
+    /// </summary>
+    public class Document
+    {
+
+    }
+}
